f04f5d3c9e8c177f062f6859c98db43ffb295aa3
benjamin zarate <benjaminzaratecaceres03@gmail.com>
2022-12-21 13:46:11 -0300

    ultimos detalles

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import { router } from "./router";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCIS1_sSuVzASIWVlx3_OVBhUYVoCnDW4w",
-  authDomain: "zarate-ecommerce-32f87.firebaseapp.com",
-  projectId: "zarate-ecommerce-32f87",
-  storageBucket: "zarate-ecommerce-32f87.appspot.com",
-  messagingSenderId: "174464631634",
-  appId: "1:174464631634:web:b112c7a5c21764cd23308b"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_,
+  appId: process.env.REACT_APP_APP_ID
 };
 
  initializeApp(firebaseConfig);
