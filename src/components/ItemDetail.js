e535a199d885c2b4298d96ef5793577948cd2a4d
Migggzz <miggz.gommz@gmail.com>
2022-12-21 18:24:10 -0600

    se modifico

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,7 +1,7 @@
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { CartContext } from "../context/cartContext";
-import { ItemCount } from "./ItemCount";
+import { ItemCount } from "./itemCount";
 
 
 const ItemDetail = ({ item }) => {
